9fcc043a6597065080f85e297c04d4c6fa0f84a0
Roshan Shah <rshah1697@gmail.com>
2018-03-25 23:56:46 +0530

    Add files via upload

---
+++ Tune in/201501244_Rutul/Assignment 5/SVD_1m.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from surprise import Reader
+from surprise import SVD
+from surprise import Dataset
+from surprise.model_selection import cross_validate
+
+if __name__ == '__main__':
+    df = pd.read_csv("data_1m.csv")
+    reader = Reader(rating_scale=(1, 5))
+    data = Dataset.load_from_df(df[['user_id', 'song_id', 'rating']], reader)
+    algo = SVD()
+    cross_validate(algo,data,measures=['RMSE'],cv=5,verbose=True)
