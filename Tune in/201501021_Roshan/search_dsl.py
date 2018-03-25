e885a1e328240e7393a7c1e19371073f85b4d3a3
Roshan Shah <rshah1697@gmail.com>
2018-03-26 02:55:19 +0530

    Delete search_dsl.py

--- Tune in/201501021_Roshan/search_dsl.py
+++
@@ -1,5 +0,0 @@
-
-from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search
-es = Elasticsearch()
-print(es.search(index="pokemon", body={"query": {"match": {'name':'Pidgey'}}}))
