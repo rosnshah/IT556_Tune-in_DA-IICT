a3830726915e60b64ae05f82d058816196832d5f
Roshan Shah <rshah1697@gmail.com>
2018-03-26 02:48:15 +0530

    Add files via upload

--- Tune in/201501113_Shrey/Assignment 5/SVD_1k.py
+++ Tune in/201501113_Shrey/Assignment 5/SVD_1k.py
@@ -3,6 +3,7 @@ from surprise import Reader
 from surprise import SVD
 from surprise import Dataset
 from surprise.model_selection import cross_validate
+from guppy import hpy
 
 if __name__ == '__main__':
     df = pd.read_csv("data_1k.csv")
@@ -10,3 +11,5 @@ if __name__ == '__main__':
     data = Dataset.load_from_df(df[['user_id', 'song_id', 'rating']], reader)
     algo = SVD()
     cross_validate(algo,data,measures=['RMSE'],cv=5,verbose=True)
+    h = hpy()
+    print h.heap()
