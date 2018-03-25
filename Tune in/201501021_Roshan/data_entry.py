66bec22d6d739525bb6efe25922daf7d55691309
Roshan Shah <rshah1697@gmail.com>
2018-03-26 02:55:14 +0530

    Delete data_entry.py

--- Tune in/201501021_Roshan/data_entry.py
+++
@@ -1,8 +0,0 @@
-import csv
-from elasticsearch import Elasticsearch, helpers
-
-es = Elasticsearch()
-
-with open('Pokemon.csv', 'rU') as f:
-    reader = csv.DictReader(f)
-    helpers.bulk(es, reader, index='pokemon', doc_type='generation1')
